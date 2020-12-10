223720f7e597f6956bd60c2404a6e0e17e7b74b0
aduque <aphous1@gmail.com>
2020-12-10 15:26:09 +0000

    html dinamic test

--- src/main/java/org/academiadecodigo/hackaton/controllers/PackageController.java
+++ src/main/java/org/academiadecodigo/hackaton/controllers/PackageController.java
@@ -1,4 +1,19 @@
 package org.academiadecodigo.hackaton.controllers;
 
+import org.academiadecodigo.hackaton.persistence.model.entertainment.Entertainment;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/packages")
 public class PackageController {
+
+    @RequestMapping(method = RequestMethod.GET, path = "/all")
+    public String showAllPackages(Model model) {
+
+        return "allPackages";
+    }
 }
