bcb811bfd806541006dad3c27c563e013862b151
aduque <aphous1@gmail.com>
2020-12-15 12:51:39 +0000

    several fixes

--- src/main/java/org/academiadecodigo/hackaton/controllers/UserController.java
+++ src/main/java/org/academiadecodigo/hackaton/controllers/UserController.java
@@ -75,7 +75,7 @@ public class UserController {
             return "user/show-edit";
         }
 
-        userService.delete(userDto.getId());
+        //userService.delete(userDto.getId());
         User savedUser = userService.add(userDtoToUser.convert(userDto));
 
         authService.setAccessingUser(savedUser);
